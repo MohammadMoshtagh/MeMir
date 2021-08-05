5042cb03e8837b53c59c26b2f7b7f639b9962ed0
Amirhosein Abedi <73402569+Mir-Abedi@users.noreply.github.com>
2021-08-05 22:17:46 +0430

    Revert "Update IndexerTest.cs"
    This reverts commit 21ad5898a79aee1f1e59f398af6ee2e82c3260fe.

--- Search-TDD-CSharp/SearchTest/IndexerTest.cs
+++ Search-TDD-CSharp/SearchTest/IndexerTest.cs
@@ -24,38 +24,37 @@ namespace SearchTest
         [Fact]
         public void Should_Index_Correctly_WhenReading_Folder()
         {
-            //MockingFolderReader();
-            //var expectedData = new HashSet<Data>();
-            //var fileNames = new []
-            //{
-            //    new HashSet<string>() {"1"},
-            //    new HashSet<string>() {"3"},
-            //    new HashSet<string>() {"4"}
-            //};
-            ////file 1
-            //expectedData.Add(MakeData(GetStem("hello"), fileNames[0]));
-            //expectedData.Add(MakeData(GetStem("dear"), fileNames[0]));
-            //expectedData.Add(MakeData(GetStem("i"), fileNames[0]));
-            //expectedData.Add(MakeData(GetStem("am"), fileNames[0]));
-            //expectedData.Add(MakeData(GetStem("mohammad"), fileNames[0]));
-            ////file 3
-            //expectedData.Add(MakeData(GetStem("man"), fileNames[1]));
-            //expectedData.Add(MakeData(GetStem("sag"), fileNames[1]));
-            //expectedData.Add(MakeData(GetStem("mikham"), fileNames[1]));
-            //expectedData.Add(MakeData(GetStem("khoshgel"), fileNames[1]));
-            //expectedData.Add(MakeData(GetStem("mikham"), fileNames[1]));
-            //expectedData.Add(MakeData(GetStem("mio"), fileNames[1]));
-            ////file 4
-            //expectedData.Add(MakeData(GetStem("mir"), fileNames[2]));
-            //expectedData.Add(MakeData(GetStem("rafte"), fileNames[2]));
-            //expectedData.Add(MakeData(GetStem("dubai"), fileNames[2]));
-            //expectedData.Add(MakeData(GetStem("vase"), fileNames[2]));
-            //expectedData.Add(MakeData(GetStem("nakhle"), fileNames[2]));
-            //expectedData.Add(MakeData(GetStem("talaii"), fileNames[2]));
+            MockingFolderReader();
+            var expectedData = new HashSet<Data>();
+            var fileNames = new []
+            {
+                new HashSet<string>() {"1"},
+                new HashSet<string>() {"3"},
+                new HashSet<string>() {"4"}
+            };
+            //file 1
+            expectedData.Add(MakeData(GetStem("hello"), fileNames[0]));
+            expectedData.Add(MakeData(GetStem("dear"), fileNames[0]));
+            expectedData.Add(MakeData(GetStem("i"), fileNames[0]));
+            expectedData.Add(MakeData(GetStem("am"), fileNames[0]));
+            expectedData.Add(MakeData(GetStem("mohammad"), fileNames[0]));
+            //file 3
+            expectedData.Add(MakeData(GetStem("man"), fileNames[1]));
+            expectedData.Add(MakeData(GetStem("sag"), fileNames[1]));
+            expectedData.Add(MakeData(GetStem("mikham"), fileNames[1]));
+            expectedData.Add(MakeData(GetStem("khoshgel"), fileNames[1]));
+            expectedData.Add(MakeData(GetStem("mikham"), fileNames[1]));
+            expectedData.Add(MakeData(GetStem("mio"), fileNames[1]));
+            //file 4
+            expectedData.Add(MakeData(GetStem("mir"), fileNames[2]));
+            expectedData.Add(MakeData(GetStem("rafte"), fileNames[2]));
+            expectedData.Add(MakeData(GetStem("dubai"), fileNames[2]));
+            expectedData.Add(MakeData(GetStem("vase"), fileNames[2]));
+            expectedData.Add(MakeData(GetStem("nakhle"), fileNames[2]));
+            expectedData.Add(MakeData(GetStem("talaii"), fileNames[2]));
 
-            //_indexer.Index("TestDataBase");
-            //Assert.Equal(expectedData, Manager.Database.GetAllData());
-			Assert.Equal(1, 1);
+            _indexer.Index("TestDataBase");
+            Assert.Equal(expectedData, Manager.Database.GetAllData());
         }
 
         private void MockingFolderReader()
